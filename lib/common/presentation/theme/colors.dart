702a4b6ea96d323827ea292f6cbd03fd86d19814
cristian2024 <cristianDRamirezM.2024@gmail.com>
2023-11-03 21:40:42 -0500

    Paquete comun de aplicación de prestamos

---
+++ lib/common/presentation/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class LoanColors {
+  ///colores oscuros
+  static const Color deepBlue = Color(0xff190482);
+  static const Color electricViolet = Color(0xff7752FE);
+  static const Color portage = Color(0xff8E8FFA);
+  static const Color periwinkle = Color(0xffC2D9FF);
+
+
+
+
+  ///colores claros
+  static const Color poloBlue = Color(0xff8EACCD);
+  static const Color kidnapper = Color(0xffD7E5CA);
+  static const Color hawkesBlue = Color(0xffD2E0FB);
+  static const Color citrineWhite = Color(0xffF9F3CC);
+}
